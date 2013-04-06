6e3f76142ec10b4a1b91a3259a30a56644c011cc
David Chanin <dchanin@quizlet.com>
2013-04-05 22:06:46 -0700

    getting audio working

--- streaming.js
+++ streaming.js
@@ -11,25 +11,30 @@ client.on('open', function() {
 
 	outputNode.onaudioprocess = function(event) {
 		if (streaming) {
-			stream.write(event.inputBuffer.getChannelData(0).buffer);
+			var rawData = event.inputBuffer.getChannelData(0);
+			var buffer = new ArrayBuffer(2 * rawData.length);
+			var view = new DataView(buffer);
+			floatTo16BitPCM(view, 0, rawData);
+			console.log(view);
+			window.view = view;
+			stream.write(view.buffer);
 		}
 	};
 
 	outputNode.connect(context.destination);
 
 	$(document).on('click', '#start-recording', function() {
-		navigator.getUserMedia(
-			{ audio: true },
-			function(s) { // success
-				console.log("Streaming!");
-				mediaStreamSource = context.createMediaStreamSource(s);
-				mediaStreamSource.connect(outputNode);
-				streaming = true;
-			}, function(event) { // failure
-				console.error(event);
-			}
-		);
+		streaming = true;
 	});
+
+	navigator.getUserMedia({ audio: true }, function(s) { // success
+			console.log("Streaming!");
+			mediaStreamSource = context.createMediaStreamSource(s);
+			mediaStreamSource.connect(outputNode);
+		}, function(event) { // failure
+			console.error(event);
+		}
+	);
 
 	$('#stop').on('click', function() {
 		console.log('Done!');
@@ -37,3 +42,11 @@ client.on('open', function() {
 		streaming = false;
 	});
 });
+
+function floatTo16BitPCM(output, offset, input){
+  for (var i = 0; i < input.length; i++, offset+=2){
+  	if (Math.abs(input[i]) > 1) console.log(input[i]);
+    var s = Math.max(-1, Math.min(1, input[i]));
+    output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+  }
+}
